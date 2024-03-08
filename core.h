b78c79bf61367d1c11de103f78492a850fb02da8
Richie Moon <richie.moon1green@gmail.com>
2024-03-08 14:08:15 +1300

    Completed core

---
+++ core.h
@@ -0,0 +1,14 @@
+//
+// Created by richi on 8/03/2024.
+//
+
+#ifndef PROJECT1_CORE_H
+#define PROJECT1_CORE_H
+
+
+class core {
+
+};
+
+
+#endif //PROJECT1_CORE_H
---
+++ main.cpp
@@ -0,0 +1,43 @@
+#include <iostream> // input-output library
+#include <fstream>
+#include <string>
+#include <math.h>  // library for sin function
+#include <vector>  // if using vectors
+#include "wav.hpp" // make sure to include this helper library
+// " " instead of <> means that library is in the same folder as your program
+
+using namespace std;
+
+// BPM: 110
+
+int main(){
+    int sample_rate = 44100; // samples per second, select value which provides good quality sound
+    double duration = 0.5; // how long [seconds] it will sound
+    double n_samples = duration * sample_rate; // if sound is "duration" seconds long and there are "sample_rate" samples per second
+    // - how many samples are there altogether? What type should this variable be?
+    double dt = duration/n_samples; // time between samples
+
+    vector<int> waveform;
+
+    int high_frequency = 1000; // pitch of the sound
+    int low_frequency = 500;
+    int volume = 6000;// 6000 is loud enough
+
+    int cycles = 4; //
+
+    for ( int i_sample = 0; i_sample < n_samples ; i_sample++){
+
+        const double pi = 3.1415926535;
+
+
+        waveform.push_back(volume * sin(2 * pi * high_frequency * i_sample * dt));
+    }
+    // generates sound file from waveform array, writes n_samples numbers
+    // into sound wav file
+    // should know sample_rate for sound timing
+    // if using vector
+    MakeWavFromVector("ambulance.wav",sample_rate, waveform); //file name can be changed but keep extension .wav
+
+    waveform.clear(); //if using vectors
+    return 0;
+}
